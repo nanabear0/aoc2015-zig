5043184d08018ba0653a62c12f73f129ab850b1f
Denizhan Erdem <derdem@microsoft.com>
2024-09-19 10:40:42 +0100

    day1 and setup

---
+++ src/day01.zig
@@ -0,0 +1,43 @@
+const std = @import("std");
+
+const input = std.mem.trim(u8, @embedFile("inputs/day01.txt"), "\n");
+
+fn part1() !void {
+    var floor: i32 = 0;
+    for (input) |c| {
+        switch (c) {
+            '(' => {
+                floor += 1;
+            },
+            ')' => {
+                floor -= 1;
+            },
+            else => unreachable,
+        }
+    }
+    std.debug.print("part1: {d}\n", .{floor});
+}
+
+fn part2() !void {
+    var floor: i32 = 0;
+    for (input, 1..) |c, i| {
+        switch (c) {
+            '(' => {
+                floor += 1;
+            },
+            ')' => {
+                floor -= 1;
+            },
+            else => unreachable,
+        }
+        if (floor < 0) {
+            std.debug.print("part2: {d}\n", .{i});
+            break;
+        }
+    }
+}
+
+pub export fn run() void {
+    part1() catch unreachable;
+    part2() catch unreachable;
+}
---
+++ src/main.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+const day01 = @import("./day01.zig");
+
+pub fn main() !void {
+    day01.run();
+}
